f44082eb43017f3f6f51c552a0d2c56db3ca6132
Akien <919824370@qq.com>
2020-04-24 14:44:06 +0800

    Basic view now returns minimal HTML

--- PythonTDD/superlists/lists/views.py
+++ PythonTDD/superlists/lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(object):
+    return HttpResponse("<html><title>To-Do lists</title></html>")
